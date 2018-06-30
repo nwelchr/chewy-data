40866dce5ba65f8cfbe06d64cd53ad1977e0fd31
nwelchr <nwelchr@gmail.com>
2018-06-29 20:09:01 -0700

    Updates

--- readme.md
+++
@@ -1,1 +0,0 @@
-[Live](https://github.com/nwelchr/chewy-data.git)
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-import data from './data.json';
-
-ReactDOM.render(<App data={data} />, document.getElementById('root'));
-registerServiceWorker();
